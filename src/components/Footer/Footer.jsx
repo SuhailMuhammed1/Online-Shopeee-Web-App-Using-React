dcfcf88ab72c3d407aca408672a9892a1b30b9a5
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-10-07 12:59:55 +0530

    name updated in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className='footer'>
-      &copy; The Cake Box {new Date().getFullYear()}
+      &copy; Online Shopeee {new Date().getFullYear()}
     </footer>
   )
 }
